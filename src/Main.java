e6f2954bf08144a0e80724c83b490c2530c5e57e
oussamaelbouenani <71403632+oussamaelbouenani@users.noreply.github.com>
2021-03-08 14:07:51 +0100

    init - projet initial

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.io.File;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+public class Main {
+	public static void main(String[] args) {
+		try {
+			File inputFile = new File("countries.xml");
+			SAXParserFactory factory = SAXParserFactory.newInstance();
+			SAXParser saxParser = factory.newSAXParser();
+			SAXHandler userhandler = new SAXHandler();
+			saxParser.parse(inputFile, userhandler);
+			Graph g = userhandler.getGraph();
+			g.calculerItineraireMinimisantNombreDeFrontieres("BEL", "IND", "output.xml");
+			g.calculerItineraireMinimisantPopulationTotale("BEL", "IND", "output2.xml");
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.exit(0);
+		}
+	}
+}
