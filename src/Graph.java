48b5e54953f61ee3932fb1e93bf7f08ede842982
oussama.elbouenani <oussama.elbouenani@student.vinci.be>
2021-03-08 14:33:42 +0100

    add - Graph.java, SAXHandler.java & gitignore

---
+++ src/Graph.java
@@ -0,0 +1,23 @@
+public class Graph {
+
+
+    /**
+     * calcule l’itinéraire entre deux pays passant par le moins de frontières possibles.
+     * @param départ pays de départ.
+     * @param arrivée pays d'arrivée.
+     * @param sortieXML nom de la sortie XML.
+     */
+    public void calculerItineraireMinimisantNombreDeFrontieres(String départ, String arrivée, String sortieXML) {
+        //TODO
+    }
+
+    /**
+     * calcule l’itinéraire entre deux pays pour lequel la somme des populations des pays traversés est la plus petite.
+     * @param départ pays de départ.
+     * @param arrivée pays d'arrivée.
+     * @param sortieXML nom de la sortie XML.
+     */
+    public void calculerItineraireMinimisantPopulationTotale(String départ, String arrivée, String sortieXML) {
+        //TODO
+    }
+}
---
+++ src/SAXHandler.java
@@ -0,0 +1,12 @@
+import org.xml.sax.helpers.DefaultHandler;
+
+public class SAXHandler extends DefaultHandler {
+
+    /**
+     * renvoie le graph créé sur base de l'input XML.
+     * @return le graph construit.
+     */
+    public Graph getGraph() {
+        return null;
+    }
+}
